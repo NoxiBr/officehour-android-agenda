7c22e411fefce73d5505e55fc9acb1a95c7c87b8
Eduardo Lagares <eduardolagares@gmail.com>
2013-01-16 16:22:44 -0200

    Aula3

---
+++ src/br/com/kiwiofficehour/agenda/ShowActivity.java
@@ -0,0 +1,11 @@
+package br.com.kiwiofficehour.agenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class ShowActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show);
+	}
+}
