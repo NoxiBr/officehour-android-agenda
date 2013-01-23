ad4373fccb16a67c11afcd1c0695a4084ea48a78
Eduardo Lagares <eduardolagares@gmail.com>
2013-01-23 18:09:38 -0200

    Aula 4

--- src/br/com/kiwiofficehour/agenda/FormActivity.java
+++ src/br/com/kiwiofficehour/agenda/FormActivity.java
@@ -1,12 +1,53 @@
 package br.com.kiwiofficehour.agenda;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.kiwiofficehour.agenda.entidades.Estado;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FormActivity extends Activity {
+	private Spinner mComboboxEstado;
+	private ArrayAdapter<Estado> mAdapter;
+	private List<Estado> mItens;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_form);
+		setContentView(R.layout.activity_form);	
+		mItens = new ArrayList<Estado>();		
+		
+		Estado e1 = new Estado();
+		e1.setId(1);
+		e1.setNome("Goiás");
+		e1.setSigla("GO");
+		mItens.add(e1);
+		
+		Estado e2 = new Estado();
+		e2.setId(2);
+		e2.setNome("São Paulo");
+		e2.setSigla("SP");
+		mItens.add(e2);
+		
+		Estado e3 = new Estado();
+		e3.setId(3);
+		e3.setNome("Belo Horizonte");
+		e3.setSigla("BH");
+		mItens.add(e3);
+		
+		Estado e4 = new Estado();
+		e4.setId(4);
+		e4.setNome("Bahia");
+		e4.setSigla("BA");
+		mItens.add(e4);
+		
+		mAdapter = new ArrayAdapter<Estado>(this, R.layout.activity_form_spinner_item,  mItens);
+		
+		mComboboxEstado = (Spinner) findViewById(R.activity_form.comboboxEstados);
+		
+		mComboboxEstado.setAdapter(mAdapter);
 	}
 }
--- src/br/com/kiwiofficehour/agenda/PrincipalActivity.java
+++ src/br/com/kiwiofficehour/agenda/PrincipalActivity.java
@@ -14,7 +14,6 @@ public class PrincipalActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_principal, menu);
         return true;
     }
